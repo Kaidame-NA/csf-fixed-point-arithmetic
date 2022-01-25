7675a7cbe4f1e463a824311754317cccbf6bb9f0
Edward Wang <ewang43@jhu.edu>
2022-01-25 18:10:22 -0500

    Remove checks for sign on creation as it is not required

--- fixedpoint.c
+++ fixedpoint.c
@@ -15,16 +15,6 @@ Fixedpoint fixedpoint_create(uint64_t whole)
     fixedPoint.whole = whole;
     fixedPoint.frac = 0;
 
-    // Determine if the value is non-negative or negative by checking most significant bit
-    if (whole & (1UL << (8 * sizeof(whole) - 1)))
-    {
-        fixedPoint.tag = VALID_NEGATIVE;
-    }
-    else
-    {
-        fixedPoint.tag = VALID_NONNEGATIVE;
-    }
-
     return fixedPoint;
 }
 
@@ -35,11 +25,6 @@ Fixedpoint fixedpoint_create2(uint64_t whole, uint64_t frac)
     fixedPoint.whole = whole;
     fixedPoint.frac = frac;
 
-    // Check most significant bit for sign
-    if (whole & (1UL << (8 * sizeof(whole) - 1)))
-    {
-        fixedPoint.tag = VALID_NEGATIVE;
-    }
     else
     {
         fixedPoint.tag = VALID_NONNEGATIVE;
