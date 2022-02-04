f4acb32aa5dda82d20cf7f9c66c4aa6193746254
Edward Wang <ewang43@jhu.edu>
2022-02-03 19:10:26 -0500

    Finish fixedpoint_is_overflow_neg

--- fixedpoint.c
+++ fixedpoint.c
@@ -264,9 +264,7 @@ int fixedpoint_is_neg(Fixedpoint val)
 
 int fixedpoint_is_overflow_neg(Fixedpoint val)
 {
-    // TODO: implement
-    assert(0);
-    return 0;
+    return val.tag == OVERFLOW_NEGATIVE;
 }
 
 int fixedpoint_is_overflow_pos(Fixedpoint val)
